bd9577a39b45dca602d1d197b217d8a73150d97a
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-03-13 01:32:22 +0300

    feat: add correct content with basic styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <>
-    <h1>Hello My name is Mahyar Shabani. I am a senior Frontend enginner with 5 years of experience</h1>
-  </>
+  return <div className={styles.wrapper}>
+    <h1>Hello, My name is Mahyar Shabani. I am a senior Frontend engineer with 5 years of experience</h1>
+  </div>
 }
