d37ebd01329bf43366292e3de727cb754eb779f4
mahyar.shabani <mahyar.shabani@ampada.com>
2023-03-21 09:18:01 +0100

    feat: add noindex to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
 
 export default function Home() {
   return <div className={styles.wrapper}>
+    <Head>
+      <title>Mahyar Shabani</title>
+      <meta name="robots" content="noindex,nofollow" />
+    </Head>
     <h1>Hello, My name is Mahyar Shabani. I am a senior Frontend engineer with 5 years of experience</h1>
   </div>
 }
