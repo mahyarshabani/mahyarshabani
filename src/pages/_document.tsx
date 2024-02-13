6c39d99cc6831f00fb0f5f8e81b64bd135faacbe
mahyar shabani <mahyar.shabani@testifi.io>
2024-02-13 16:56:15 +0100

    add sample csp v2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-    const csp = `frame-src 'https://pol.qa.testifi.io/'`;
-
     return (
         <Html lang="en">
-            <Head>
-                <meta httpEquiv="Content-Security-Policy" content={csp} />
-            </Head>
+            <Head/>
             <body>
                 <Main />
                 <NextScript />
